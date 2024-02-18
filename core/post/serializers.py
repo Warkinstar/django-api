d9961bbb1082f358cc7ed3b456e386bb7ca0e159
Ramil <Warkinstar@mail.ru>
2024-02-18 20:37:02 +0600

    Adding the likes_count and has_liked fields to PostSerializer

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -9,6 +9,8 @@ from core.user.models import User
 
 class PostSerializer(AbstractSerializer):
     author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field="public_id")
+    liked = serializers.SerializerMethodField()  # связан с get_liked get_<field_name>
+    likes_count = serializers.SerializerMethodField()  # связан get_likes_count get_<field_name>
 
     def validate_author(self, value):
         if self.context["request"].user != value:
@@ -28,6 +30,16 @@ class PostSerializer(AbstractSerializer):
         author = User.objects.get_object_by_public_id(rep["author"])
         rep["author"] = UserSerializer(author).data
         return rep
+
+    def get_liked(self, instance):
+        request = self.context.get("request", None)
+        if request is None or request.user.is_anonymous:
+            return False
+
+        return request.user.has_liked(instance)
+
+    def get_likes_count(self, instance):
+        return instance.liked_by.count()
 
     class Meta:
         model = Post
