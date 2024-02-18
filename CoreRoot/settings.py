a9b56f4d98d20a0c41afdea44b99463dfbbd637a
Ramil <Warkinstar@mail.ru>
2024-02-18 14:47:47 +0600

    LimitOffsetPagination
    Как работает: Клиенты используют параметры запроса limit и offset, чтобы определить, сколько элементов данных они хотят получить и с какого момента начать. Например, ?limit=10&offset=20 вернет 10 элементов, начиная с 21-го.

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -144,4 +144,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+
+    # ?limit=10&offset=20 - 10 элементов, начиная с 21
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 15,
 }
